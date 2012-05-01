86eda968fcc77e799670c761588fbd8dbfa0197d
Henk Muller <henk@xmos.com>
2012-05-01 11:07:10 +0100

    Tested the timer

--- app_c_example/src/main.c
+++ app_c_example/src/main.c
@@ -1,2 +1,18 @@
-main() {
+#include "timer.h"
+
+void timertest() {
+    timer_t x;
+    int t;
+
+    timer_init(x);
+    timer_in(x, t);
+    t += 100;
+    timer_in_when_timerafter(x, t, t);
+    timer_exit(x);
+}
+
+
+int main(void) {
+    timertest();
+    return 0;
 }
--- module_c_xcore_resources/src/timer.h
+++ module_c_xcore_resources/src/timer.h
@@ -2,20 +2,20 @@
 
 #define C_TIMER_H
 
-typedef timer_t int;
+typedef int timer_t;
 
 #define timer_init(t) \
-    asm("getr %0,1" : "=r" (t));
+    asm volatile("getr %0,1" : "=r" (t));
 
 #define timer_exit(t) \
-    asm("freer res[%0]" :: "r" (t));
+    asm volatile("freer res[%0]" :: "r" (t));
 
 #define timer_in(t, i)                            \
-    asm("in %0,res[%1]" : "=r" (i): "r" (t));
+    asm volatile("in %0,res[%1]" : "=r" (i): "r" (t));
 
-#define port_in_when_timerafter(t,i,v)              \
-    asm("setd res[%0],%1" :: "r" (t), "r" (v)); \
-    asm("setc res[%0],9" :: "r" (t));       \
-    asm("in %0,res[%1]" : "=r" (i): "r" (t));
+#define timer_in_when_timerafter(t,i,v)              \
+    asm volatile("setd res[%0],%1" :: "r" (t), "r" (v)); \
+    asm volatile("setc res[%0],9" :: "r" (t));       \
+    asm volatile("in %0,res[%1]" : "=r" (i): "r" (t));
 
 #endif
